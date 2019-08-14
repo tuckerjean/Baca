a02d71a7f262fc8b5a8b0a30d72b964f4534e081
Jean Tucker <52928553+tuckerjean@users.noreply.github.com>
2019-08-14 15:58:00 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$uid="Facebook_2583607791658939";
+$raw_uid="866ef0e4-46ec-4530-9210-2a229f802165";
+$user_name="Cahya+Septiawan";
+$invite_code="FBpdrZG0rr";
+$user_token="AbyWHMFmL5EydPSt";
+$own_mac="02%3A00%3A00%3A00%3A00%3A00";
+$android_id="1a02e7988082aeb8";
+$device_type="Xiomi++Redmi+5+Plus";
+
+
+?>
